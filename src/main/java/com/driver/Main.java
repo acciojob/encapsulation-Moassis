e3fcabc8f9d7c1b2e7dcd91a44ad65643d3d5ba5
Moassis <moassis.rommel@gmail.com>
2022-12-11 18:50:44 +0530

    message1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Rommel");
+
+        // name is declared private, so it is showing error,
+        // we need to remove private or call the name by getter function
+
+        System.out.println(obj.getName());
+    }
+
 }
